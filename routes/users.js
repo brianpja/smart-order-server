7e9357661945359d3faedaa69d055e8a54a017ed
Brian James <brianpja@gmail.com>
2017-10-13 11:00:04 -0700

    add new user working

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
+const bcrypt = require('bcrypt-as-promised')
 
 router.get('/users', (req, res, next) => {
   knex('users')
@@ -11,6 +12,26 @@ router.get('/users', (req, res, next) => {
     })
     .catch((err) => {
       next(err);
+    })
+})
+
+router.post('/users', (req, res, next) => {
+  console.log(req.body)
+  const userData = req.body;
+
+  console.log(userData)
+  bcrypt.hash(userData.password, 12)
+    .then(function(hashed_password) {
+      delete userData.password;
+      userData.hashed_password = hashed_password;
+      console.log('after hash:', userData)
+
+      knex('users')
+        .insert(userData, '*')
+        .then(function(result) {
+          console.log(result);
+          res.send(result[0])
+        })
     })
 })
 
